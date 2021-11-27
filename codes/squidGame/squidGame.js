adf6cc2d463b0ae99ecf6d196a45693b6f942724
devAdam117 <adam.martinka99@gmail.com>
2021-11-27 22:14:56 +0100

    squidGame

--- codes/squidGame/squidGame.js
+++ codes/squidGame/squidGame.js
@@ -3,30 +3,53 @@ const chalk = require("chalk");
 const fs = require('fs');
 const arrayToTxtFile = require('array-to-txt-file');
 
-let numOfPlayers = 16;
-let numOfGlassPairs = 15;
-let yourNum = 16;
-let players=  [];
 
-class Player  {
-    constructor(id,deaths,status){
-        this.id=id;  // poradie              
-        this.deaths=deaths;
-        this.status=status
-    }
-}
-const createPlayers = (num)=> {
-    
-    for(let i = 0 ; i<num ; i++){
-        players.push(new Player(i,0,"start"));
-    }
+
+let stats = {
+    nEscapee:[],
+    nPairs:[]
 }
 
-const startGame = (numOfPlayers,numOfGlassPairs) => {
-    createPlayers(numOfPlayers);
-    let currentGlass = 0;
-    while(currentGlass < numOfGlassPairs  ){
-        //handle round bude zvysoavt currentGlass, potom bude hracom pridavat na deadoch a takisto im bude menit status na status dead/survived
 
+
+const startGame = (nPlayers,nPairs,nSim) => {
+    let livingPlayers;
+    let unknownPairs;
+    for(let i =0 ; i < nSim ; i++){
+        livingPlayers= nPlayers;
+        unknownPairs = nPairs;
+        while(livingPlayers>0 && unknownPairs>0){
+            let decidingJump = Math.random();
+            if(decidingJump>0.5){
+                unknownPairs --;        
+            }
+            else {
+                unknownPairs --;
+                livingPlayers --;
+            }
+        }        
+        stats.nEscapee.push(livingPlayers);
+        stats.nPairs.push(unknownPairs);
     }
 }
+
+let initPlayers = 30;
+let initGlassPairs = 70;
+let nSim = 100000;
+
+startGame(initPlayers,initGlassPairs,nSim);
+arrayToTxtFile([stats.nEscapee], './output.txt', err => {
+    if(err) {
+      console.error(err)
+      return
+    }
+    console.log('Successfully wrote to txt file')
+})
+console.log(chalk.blueBright("Abortion of escapees:"))
+console.table(functions.showAbortion(stats.nEscapee))
+console.log(chalk.blueBright("Abortion of unknown pairs:"))
+console.table(functions.showAbortion(stats.nPairs))
+
+
+
+
